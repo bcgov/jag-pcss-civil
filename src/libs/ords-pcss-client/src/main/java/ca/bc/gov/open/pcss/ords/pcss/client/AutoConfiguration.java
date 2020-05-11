2308aa929290c5293a19367d22ca6f8bec096999
alex <alex.joyeux@gov.bc.ca>
2020-05-11 08:24:10 -0700

    Adding auto-gen ords client

---
+++ src/libs/ords-pcss-client/src/main/java/ca/bc/gov/open/pcss/ords/pcss/client/AutoConfiguration.java
@@ -0,0 +1,42 @@
+package ca.bc.gov.open.pcss.ords.pcss.client;
+
+import ca.bc.gov.open.pcss.ords.pcss.client.api.PcssApi;
+import ca.bc.gov.open.pcss.ords.pcss.client.api.handler.ApiClient;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(OrdsProperties.class)
+public class AutoConfiguration {
+
+    private final OrdsProperties ordsProperties;
+
+    public AutoConfiguration(OrdsProperties ordsProperties) {
+        this.ordsProperties = ordsProperties;
+    }
+
+    @Bean(name = "pcssApiClient")
+    public ApiClient apiClient() {
+        ApiClient apiClient = new ApiClient();
+
+        apiClient.setBasePath(ordsProperties.getBasePath());
+
+        if(StringUtils.isNotBlank(ordsProperties.getUsername()))
+            apiClient.setUsername(ordsProperties.getUsername());
+
+        if(StringUtils.isNotBlank(ordsProperties.getPassword()))
+            apiClient.setPassword(ordsProperties.getPassword());
+
+        return apiClient;
+    }
+
+    @Bean
+    public PcssApi pcssApi(@Qualifier("pcssApiClient") ApiClient apiClient) {
+        return new PcssApi(apiClient);
+    }
+
+}
+
