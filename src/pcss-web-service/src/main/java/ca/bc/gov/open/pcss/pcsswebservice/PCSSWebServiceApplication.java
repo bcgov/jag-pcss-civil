16e5a672ef165be9376352518fcd02f1a54eb01b
Kyle Flood <Kyle.Flood@nttdata.com>
2020-06-15 07:08:57 -0700

    JBoss Configure
    Building as WAR, dockerfile setup for JBoss-EAP-7.3

--- src/pcss-web-service/src/main/java/ca/bc/gov/open/pcss/pcsswebservice/PCSSWebServiceApplication.java
+++ src/pcss-web-service/src/main/java/ca/bc/gov/open/pcss/pcsswebservice/PCSSWebServiceApplication.java
@@ -2,9 +2,16 @@ package ca.bc.gov.open.pcss.pcsswebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PCSSWebServiceApplication {
+public class PCSSWebServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PCSSWebServiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PCSSWebServiceApplication.class, args);
