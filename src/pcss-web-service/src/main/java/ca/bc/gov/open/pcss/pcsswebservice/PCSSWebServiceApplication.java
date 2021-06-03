20c18ee97d26f646e3be22cead623ac3a47792dd
Zac <zacpez@gmail.com>
2021-06-03 09:35:24 -0700

    refactor: removed JBoss application configuration, updated docs

--- src/pcss-web-service/src/main/java/ca/bc/gov/open/pcss/pcsswebservice/PCSSWebServiceApplication.java
+++ src/pcss-web-service/src/main/java/ca/bc/gov/open/pcss/pcsswebservice/PCSSWebServiceApplication.java
@@ -2,16 +2,9 @@ package ca.bc.gov.open.pcss.pcsswebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PCSSWebServiceApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(PCSSWebServiceApplication.class);
-	}
+public class PCSSWebServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PCSSWebServiceApplication.class, args);
