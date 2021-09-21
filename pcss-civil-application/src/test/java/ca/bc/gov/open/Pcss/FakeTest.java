c5d9a27fdd25312bdf17acb8e134065ee4636fc9
ekwanNTT <ethan.kwan@nttdata.com>
2021-09-21 14:38:01 -0700

    code style

--- pcss-civil-application/src/test/java/ca/bc/gov/open/Pcss/FakeTest.java
+++ pcss-civil-application/src/test/java/ca/bc/gov/open/Pcss/FakeTest.java
@@ -1,5 +1,7 @@
 package ca.bc.gov.open.Pcss;
 
+import static org.mockito.Mockito.when;
+
 import ca.bc.gov.open.Pcss.Controllers.AppearanceController;
 import com.example.demp.wsdl.pcss.three.GetAppearanceCivil;
 import org.junit.jupiter.api.Test;
@@ -13,26 +15,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestTemplate;
 
-import static org.mockito.Mockito.when;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class FakeTest {
 
-    @Mock
-    private RestTemplate restTemplate = new RestTemplate();
+    @Mock private RestTemplate restTemplate = new RestTemplate();
 
     @Test
     public void test() {
         AppearanceController controller = new AppearanceController(restTemplate);
         ResponseEntity<com.example.demp.wsdl.pcss.one.GetAppearanceCivilResponse> responseEntity =
-                new ResponseEntity<>(new com.example.demp.wsdl.pcss.one.GetAppearanceCivilResponse(), HttpStatus.OK);
+                new ResponseEntity<>(
+                        new com.example.demp.wsdl.pcss.one.GetAppearanceCivilResponse(),
+                        HttpStatus.OK);
 
         when(restTemplate.exchange(
-                Mockito.any(String.class),
-                Mockito.eq(HttpMethod.GET),
-                Mockito.<HttpEntity<String>>any(),
-                Mockito.<Class<com.example.demp.wsdl.pcss.one.GetAppearanceCivilResponse>>any()))
+                        Mockito.any(String.class),
+                        Mockito.eq(HttpMethod.GET),
+                        Mockito.<HttpEntity<String>>any(),
+                        Mockito
+                                .<Class<com.example.demp.wsdl.pcss.one.GetAppearanceCivilResponse>>
+                                        any()))
                 .thenReturn(responseEntity);
 
         var out = controller.getAppearanceCivil(new GetAppearanceCivil());
