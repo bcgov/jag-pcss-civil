28396344012574ab60a2dd94ebf97aa69b60cb5f
ekwanNTT <ethan.kwan@nttdata.com>
2021-09-21 10:06:19 -0700

    root coverage generation is working

---
+++ pcss-civil-application/src/test/java/ca/bc/gov/open/Pcss/FakeTest.java
@@ -0,0 +1,14 @@
+package ca.bc.gov.open.Pcss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FakeTest {
+
+
+    @Test
+    public void test() {
+        assert true;
+    }
+}
