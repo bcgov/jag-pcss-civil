8cb286efc68b4077b6114b6bd7acc3111e43bf61
ekwanNTT <ethan.kwan@nttdata.com>
2021-09-21 10:23:48 -0700

    code style fix

--- pcss-civil-application/src/test/java/ca/bc/gov/open/Pcss/FakeTest.java
+++ pcss-civil-application/src/test/java/ca/bc/gov/open/Pcss/FakeTest.java
@@ -2,8 +2,10 @@ package ca.bc.gov.open.Pcss;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class FakeTest {
 
     @Test
