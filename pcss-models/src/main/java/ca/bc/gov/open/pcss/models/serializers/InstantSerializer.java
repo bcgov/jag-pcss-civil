b30539d9993bb01a6f14d30b2b0e532ceaf80473
Sylar Z. Zhang <Sylar.Zhang@gov.bc.ca>
2022-01-18 13:33:37 -0800

    Cleanup
    - Removed unused methods

--- pcss-models/src/main/java/ca/bc/gov/open/pcss/models/serializers/InstantSerializer.java
+++ pcss-models/src/main/java/ca/bc/gov/open/pcss/models/serializers/InstantSerializer.java
@@ -21,27 +21,12 @@ public class InstantSerializer extends JsonSerializer<Instant> {
         gen.writeString(out);
     }
 
-    public static String convert2(Instant value) {
-        return value.toString().replace("T", " ").replace("Z", ".0");
-    }
-
     public static String convert(Instant value) {
         if (value == null) {
             return null;
         }
 
         return DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss")
-                .withZone(ZoneId.of("GMT-7"))
-                .withLocale(Locale.US)
-                .format(value);
-    }
-
-    public static String convert3(Instant value) {
-        if (value == null) {
-            return null;
-        }
-
-        return DateTimeFormatter.ofPattern("dd-MMM-yy")
                 .withZone(ZoneId.of("GMT-7"))
                 .withLocale(Locale.US)
                 .format(value);
