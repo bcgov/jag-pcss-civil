f2fdeb88fa5c0c097ba531b0f66dc3cfd0dde0cd
ekwan <224302@nttdata.com>
2022-02-01 11:44:40 -0800

    clean up API working

--- comparison-tool/src/main/java/ca/bc/gov/open/pcss/civil/comparison/services/TestService.java
+++ comparison-tool/src/main/java/ca/bc/gov/open/pcss/civil/comparison/services/TestService.java
@@ -28,84 +28,54 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 
-
 @Service
 public class TestService {
-    private RestTemplate restTemplate = new RestTemplate();
-    private WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
+  private RestTemplate restTemplate = new RestTemplate();
+  private WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
 
-    private WebServiceSenderWithAuth webServiceSenderWithAuth;
+  private WebServiceSenderWithAuth webServiceSenderWithAuth;
 
-    @Autowired
-    public TestService(WebServiceSenderWithAuth webServiceSenderWithAuth) {
-        this.webServiceSenderWithAuth = webServiceSenderWithAuth;
-    }
+  @Autowired
+  public TestService(WebServiceSenderWithAuth webServiceSenderWithAuth) {
+    this.webServiceSenderWithAuth = webServiceSenderWithAuth;
+  }
 
-    @Value("${host.api_host}")
-    private String apiHost;
+  @Value("${host.api_host}")
+  private String apiHost;
 
-    @Value("${host.wm_host}")
-    private String wmHost;
+  @Value("${host.wm_host}")
+  private String wmHost;
 
-    @Value("${host.username}")
-    private String username;
+  @Value("${host.username}")
+  private String username;
 
-    @Value("${host.password}")
-    private String password;
+  @Value("${host.password}")
+  private String password;
 
-    public <T, G> boolean compare(T response, G request)
-            throws JAXBException, SOAPException, IOException {
-        GetFileDetailCivil getFileDetailCivil;
+  public <T, G> boolean compare(T response, G request)
+      throws SOAPException {
 
-        UriComponentsBuilder builder =
-                UriComponentsBuilder.fromHttpUrl(apiHost);
 
-        JAXBContext jaxbContext = JAXBContext.newInstance(request.getClass());
-        Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
-        StringWriter stringWriter = new StringWriter();
+    Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
 
-        MessageFactory messageFactory = MessageFactory.newInstance(SOAPConstants.SOAP_1_2_PROTOCOL);
-        SOAPMessage soapMessage = messageFactory.createMessage();
-        SOAPPart soapPart = soapMessage.getSOAPPart();
-        SOAPEnvelope soapEnvelope = soapPart.getEnvelope();
-        SOAPBody soapBody = soapEnvelope.getBody();
-        SaajSoapMessageFactory saajSoapMessageFactory = new SaajSoapMessageFactory(messageFactory);
-        saajSoapMessageFactory.afterPropertiesSet();
+    MessageFactory messageFactory = MessageFactory.newInstance(SOAPConstants.SOAP_1_2_PROTOCOL);
 
-        // jaxb2Marshaller.marshal(request, soapBody);
-        ByteOutputStream byteOutputStream = new ByteOutputStream();
-        soapMessage.writeTo(byteOutputStream);
+    SaajSoapMessageFactory saajSoapMessageFactory = new SaajSoapMessageFactory(messageFactory);
+    saajSoapMessageFactory.afterPropertiesSet();
 
-//        HttpHeaders headers = new HttpHeaders();
-//        String auth = username + ":" + password;
-//        headers.setBasicAuth(username, password);
-//
-//        headers.setContentType(new MediaType("application", "soap+xml"));
-//        HttpEntity<String> payload = new HttpEntity<>(
-//                new String(byteOutputStream.getBytes()), headers);
+    HttpComponentsMessageSender httpComponentsMessageSender = new HttpComponentsMessageSender();
+    httpComponentsMessageSender.setCredentials(new UsernamePasswordCredentials(username, password));
 
-//        ResponseEntity<String> responseApi = restTemplate.postForEntity(
-//                                                            builder.toUriString(),
-//                                                            payload,
-//                                                            String.class
-//                                                            );
+    webServiceTemplate.setMessageSender(webServiceSenderWithAuth);
+    webServiceTemplate.setMessageFactory(saajSoapMessageFactory);
+    jaxb2Marshaller.setContextPaths("ca.bc.gov.open.pcss.one", "ca.bc.gov.open.pcss.three");
+    webServiceTemplate.setMarshaller(jaxb2Marshaller);
+    webServiceTemplate.setUnmarshaller(jaxb2Marshaller);
+    webServiceTemplate.afterPropertiesSet();
 
-        StreamSource streamSource = new StreamSource(new StringReader(new String(byteOutputStream.toString())));
-        StreamResult streamResult = new StreamResult(stringWriter);
+    T resultObjectWM = (T) webServiceTemplate.marshalSendAndReceive(wmHost, request);
+    T resultObjectAPI = (T) webServiceTemplate.marshalSendAndReceive(apiHost, request);
 
-        HttpComponentsMessageSender httpComponentsMessageSender = new HttpComponentsMessageSender();
-        httpComponentsMessageSender.setCredentials(new UsernamePasswordCredentials(username, password));
-
-        webServiceTemplate.setMessageSender(webServiceSenderWithAuth);
-//        webServiceTemplate.sendSourceAndReceiveToResult(builder.toUriString(),
-//                                                      streamSource,
-//                                                      streamResult
-//                                                      );
-        jaxb2Marshaller.setContextPaths("ca.bc.gov.open.pcss.one", "ca.bc.gov.open.pcss.three");
-        webServiceTemplate.setMarshaller(jaxb2Marshaller);
-        webServiceTemplate.afterPropertiesSet();
-        Object resultObject = webServiceTemplate.marshalSendAndReceive(builder.toUriString(), request);
-
-        return false;
-    }
+    return false;
+  }
 }
