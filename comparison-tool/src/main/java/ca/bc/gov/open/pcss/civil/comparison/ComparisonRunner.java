94d0c35630247a90df6945abada332ccd7392445
ekwan <224302@nttdata.com>
2022-02-03 09:53:04 -0800

    shutdown after run

--- comparison-tool/src/main/java/ca/bc/gov/open/pcss/civil/comparison/ComparisonRunner.java
+++ comparison-tool/src/main/java/ca/bc/gov/open/pcss/civil/comparison/ComparisonRunner.java
@@ -5,20 +5,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ComparisonRunner {
+    private static ConfigurableApplicationContext ctx;
     @Autowired private TestService testService;
 
     public static void main(String args[]) {
-        SpringApplication.run(ComparisonRunner.class, args);
+        ctx = SpringApplication.run(ComparisonRunner.class, args);
     }
 
     @Bean
     public CommandLineRunner CommandLineRunnerBean() {
         return (args) -> {
             testService.runCompares();
+
+            System.exit(SpringApplication.exit(ctx, () -> 0));
         };
     }
 }
