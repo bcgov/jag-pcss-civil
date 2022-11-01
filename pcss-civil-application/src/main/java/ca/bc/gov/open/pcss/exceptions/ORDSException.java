6b4e4d298c3aa27a8ddf9a2f7455fb5bf6371325
jianming tu <tu_jm@yahoo.com>
2022-11-01 12:41:25 -0700

    change the faultCode from CLIENT to SERVER to return status code 500 back to SOAP UI

--- pcss-civil-application/src/main/java/ca/bc/gov/open/pcss/exceptions/ORDSException.java
+++ pcss-civil-application/src/main/java/ca/bc/gov/open/pcss/exceptions/ORDSException.java
@@ -4,7 +4,7 @@ import org.springframework.ws.soap.server.endpoint.annotation.FaultCode;
 import org.springframework.ws.soap.server.endpoint.annotation.SoapFault;
 
 @SoapFault(
-        faultCode = FaultCode.CLIENT,
+        faultCode = FaultCode.SERVER,
         faultStringOrReason =
                 "An error response was received from ORDS please check that your request is of valid form")
 public class ORDSException extends RuntimeException {
